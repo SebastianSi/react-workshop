2ee8246e110aa544b469a872114c2e63b8af37a4
Stefan Chira <stefan.chira@3pillarglobal.com>
2019-03-11 13:21:06 +0200

    first redux commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,24 @@ import './styles/App.css';
 import UsersList from './components/UsersList';
 import Notfound from './components/staticPages/NotFound';
 import ContactPage from './components/staticPages/Contact';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
         <Router>
-          <div className='App'>
-              <Switch>
-                <Route path='/' exact component={UsersList} />
-                <Route path='/contact/' exact component={ContactPage} />
+          <div className="App">
+            <Switch>
+              <Route path="/" exact component={UsersList} />
+              <Route path="/contact/" exact component={ContactPage} />
 
-                <Route component={Notfound}/>
-              </Switch>
+              <Route component={Notfound} />
+            </Switch>
           </div>
         </Router>
+      </Provider>
     );
   }
 }
