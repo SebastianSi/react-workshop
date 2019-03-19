6943ed07114f92d82f446b4d6d396ee171f4bb8b
Stefan Chira <stefan.chira@3pillarglobal.com>
2019-03-20 01:12:41 +0200

    redux-01 small refactoring

--- src/store.js
+++ src/store.js
@@ -1,11 +1,7 @@
 import { createStore } from 'redux';
 import rootReducer from './reducers';
 
-// !!! comment to use REDUX_DEVTOOLS_EXTENSION
 export const store = createStore(rootReducer);
 
-// !!! uncomment to use REDUX_DEVTOOLS_EXTENSION
-// export const store = createStore(
-//   rootReducer,
-//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-// );
+// with REDUX_DEVTOOLS_EXTENSION
+// export const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
