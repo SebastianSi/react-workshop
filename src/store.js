aeb91d29a8b71fbfa62762366fffabf406104da1
Stefan Chira <stefan.chira@3pillarglobal.com>
2019-03-21 02:12:30 +0200

    small chnage

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { createStore } from 'redux';
 import rootReducer from './reducers';
 
-// export const store = createStore(rootReducer);
+export const store = createStore(rootReducer);
 
 // with REDUX_DEVTOOLS_EXTENSION
-export const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+// export const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
