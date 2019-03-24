787acd21ddd12ce4f4ab500c5cd233456fa9a722
Stefan Chira <stefan.chira@3pillarglobal.com>
2019-03-24 21:47:17 +0200

    refactoring

--- src/components/ResetSnackbar.js
+++ src/components/ResetSnackbar.js
@@ -1,65 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Button from '@material-ui/core/Button';
 import Snackbar from '@material-ui/core/Snackbar';
 import { connect } from 'react-redux';
 
 import { closeResetPostFormSnackbar } from '../actions/postsActions';
 
-class ResetSnackbar extends Component {
-  state = {
-    open: false
-  };
-
-  handleClick = () => {
-    this.setState({ open: true });
-  };
-
-  handleClose = (event, reason) => {
-    if (reason === 'clickaway') {
-      return;
-    }
-
-    this.setState({ open: false });
-  };
-
-  render() {
-    const { open, closeSnackbar } = this.props;
-
-    return (
-      <div>
-        <Snackbar
-          anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'left'
-          }}
-          open={open}
-          onClose={closeSnackbar}
-          message={<span id="message-id">Are you sure you want to reset the form?</span>}
-          action={[
-            <Button
-              key="undo"
-              color="primary"
-              size="small"
-              onClick={(event, reason) => {
-                closeSnackbar(event, reason, false);
-              }}>
-              UNDO
-            </Button>,
-            <Button
-              key="continue"
-              color="secondary"
-              size="small"
-              onClick={(event, reason) => {
-                closeSnackbar(event, reason, true);
-              }}>
-              CONTINUE
-            </Button>
-          ]}
-        />
-      </div>
-    );
-  }
-}
+const ResetSnackbar = ({ open, closeSnackbar }) => (
+  <div>
+    <Snackbar
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'left'
+      }}
+      open={open}
+      onClose={closeSnackbar}
+      message={<span id="message-id">Are you sure you want to reset the form?</span>}
+      action={[
+        <Button
+          key="undo"
+          color="primary"
+          size="small"
+          onClick={(event, reason) => {
+            closeSnackbar(event, reason, false);
+          }}>
+          UNDO
+        </Button>,
+        <Button
+          key="continue"
+          color="secondary"
+          size="small"
+          onClick={(event, reason) => {
+            closeSnackbar(event, reason, true);
+          }}>
+          CONTINUE
+        </Button>
+      ]}
+    />
+  </div>
+);
 
 const mapStateToProps = state => ({
   open: state.postsReducer.openResetPostFormSnackbar
